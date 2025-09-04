87d030ff57bf646052ab6844dd481831f722f095
Kamal Swarnkar <swarnkar889@gmail.com>
2025-09-04 18:03:05 +0530

    almost done

--- TaleOn/taleon-backend/src/config/passport.js
+++ TaleOn/taleon-backend/src/config/passport.js
@@ -20,7 +20,7 @@ const configureGoogleStrategy = () => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback',
+        callbackURL: process.env.GOOGLE_CALLBACK_URL,
         scope: ['profile', 'email'],
       },
       async (accessToken, refreshToken, profile, done) => {
